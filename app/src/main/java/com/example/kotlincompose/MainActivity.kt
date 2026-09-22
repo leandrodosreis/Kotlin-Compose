4f1995d1ed8fdb9a29eb53fc5ce6cf295cfb3420
LEANDRO DOS REIS FILHO <25203700@dsn127m1246734.cfp127aluno.local>
2026-09-22 14:09:42 -0300

    Mais sobre Kotlin Compose

--- app/src/main/java/com/example/kotlincompose/MainActivity.kt
+++ app/src/main/java/com/example/kotlincompose/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlincompose.ui.theme.KotlinComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +38,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting( ) {
 
-    Column() {
+    Column(
+        modifier = Modifier.padding(32.dp).fillMaxWidth()
+    ) {
 
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = "",
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Red
+            ),
             onValueChange = {},
             placeholder = {Text( text = "Oque voce esta procurando")},
             trailingIcon = {
